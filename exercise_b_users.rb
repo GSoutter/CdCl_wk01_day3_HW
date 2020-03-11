a7e687d4d88306cf7ac45754177ca05806c7db27
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-11 20:15:55 +0000

    adds answers for b1 to b7

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -55,12 +55,40 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p "Johnathans twitter = #{users["Jonathan"][:twitter]}"
+
 # 2. Get Erik's hometown
+p "Eriks home town  = #{users["Erik"][:home_town]}"
+
 # 3. Get the array of Erik's lottery numbers
+p "Eriks lottery number = #{users["Erik"][:lottery_numbers]}"
+
+
 # 4. Get the species of Avril's pet Monty
+p "Avrils species #{users["Avril"][:pets][0][:species]}"
+
 # 5. Get the smallest of Erik's lottery numbers
+ "Eriks lowest number = #{}"
+
+p "Eriks lowest number = #{users["Erik"][:lottery_numbers].min()}"
+
 # 6. Return an array of Avril's lottery numbers that are even
+avrils_odd_nums = []
+for num in users["Avril"][:lottery_numbers]
+  if (num % 2 == 0)
+    avrils_odd_nums.push(num)
+  end
+end
+
+p  "Avrils even lottery numbers = #{avrils_odd_nums}"
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers].push(7)
+
+p "Eriks lottery number with 7 = #{users["Erik"][:lottery_numbers]}"
+
 # 8. Change Erik's hometown to Edinburgh
+
 # 9. Add a pet dog to Erik called "Fluffy"
+
 # 10. Add another person to the users hash
